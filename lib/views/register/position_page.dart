05263dc1c2ec6b0c2b8dbce06b4d2b604ed82d72
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2023-04-01 19:34:31 +0900

    兄弟構成

--- lib/views/register/position_page.dart
+++ lib/views/register/position_page.dart
@@ -16,35 +16,85 @@ class _PositionPageState extends State<PositionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Your Position'),
+      ),
       body: SafeArea(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 50.0,
             ),
-            RadioListTile(
-              title: Text('長子'),
-              value: PositionValue.Eldest,
-              groupValue: _positionValue,
-              onChanged: (value) => _onRadioSelected(value),
+            Card(
+              color: Colors.white,
+              child: Column(children: [
+                RadioListTile(
+                  title: const Text('Eldest'),
+                  value: PositionValue.Eldest,
+                  groupValue: _positionValue,
+                  onChanged: (value) => _onRadioSelected(value),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.person, size: 48, color: Colors.red),
+                    Icon(Icons.person, size: 36, color: Colors.black),
+                    Icon(Icons.person, size: 30, color: Colors.black),
+                  ],
+                ),
+              ]),
             ),
-            RadioListTile(
-              title: Text('中間子'),
-              value: PositionValue.Middle,
-              groupValue: _positionValue,
-              onChanged: (value) => _onRadioSelected(value),
+            Card(
+              child: Column(children: [
+                RadioListTile(
+                  title: const Text('Middle'),
+                  value: PositionValue.Middle,
+                  groupValue: _positionValue,
+                  onChanged: (value) => _onRadioSelected(value),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.person, size: 48, color: Colors.black),
+                    Icon(Icons.person, size: 36, color: Colors.red),
+                    Icon(Icons.person, size: 30, color: Colors.black),
+                  ],
+                ),
+              ]),
             ),
-            RadioListTile(
-              title: Text('末子'),
-              value: PositionValue.Youngest,
-              groupValue: _positionValue,
-              onChanged: (value) => _onRadioSelected(value),
+            Card(
+              child: Column(children: [
+                RadioListTile(
+                  title: const Text('Youngest'),
+                  value: PositionValue.Youngest,
+                  groupValue: _positionValue,
+                  onChanged: (value) => _onRadioSelected(value),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.person, size: 48, color: Colors.black),
+                    Icon(Icons.person, size: 36, color: Colors.black),
+                    Icon(Icons.person, size: 30, color: Colors.red),
+                  ],
+                ),
+              ]),
             ),
-            RadioListTile(
-              title: Text('一人っ子'),
-              value: PositionValue.Only,
-              groupValue: _positionValue,
-              onChanged: (value) => _onRadioSelected(value),
+            Card(
+              child: Column(children: [
+                RadioListTile(
+                  title: const Text('Only'),
+                  value: PositionValue.Only,
+                  groupValue: _positionValue,
+                  onChanged: (value) => _onRadioSelected(value),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.person, size: 48, color: Colors.red),
+                  ],
+                ),
+              ]),
             ),
           ],
         ),
