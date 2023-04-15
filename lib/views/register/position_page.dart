3299adf5efda89b38617abd4f0d51ae8e10d40ea
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2023-04-15 16:18:11 +0900

    base create

--- lib/views/register/position_page.dart
+++ lib/views/register/position_page.dart
@@ -1,7 +1,6 @@
 //兄弟構成
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:zajonc/constants/snackbar.dart';
 
